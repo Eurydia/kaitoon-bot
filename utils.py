da7bb015f84f42e7e388fbe234289080d7e6fbb6
Eurydia <84164836+Eurydia@users.noreply.github.com>
2021-09-27 18:55:14 +0700

    m

--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
-import enum
-from operator import sub
 from typing import Tuple, Dict
 
 
 from chempy.chemistry import Substance, Reaction, Equilibrium
-# from chempy.chemistry import balance_stoichiometry
+from chempy.chemistry import balance_stoichiometry
 
 
 def replace_square_bracket(raw: str) -> str:
@@ -37,13 +35,33 @@ def format_reaction(reactants: Dict, products: Dict) -> str:
 def simple_balance(reactants: str, products: str):    
     reactants, products = map(replace_square_bracket, (reactants, products))
     reactants, products = remove_duplicate(reactants, products)
-    # return balance_stoichiometry(reactants, products)
+    return balance_stoichiometry(reactants, products)
 
 
 substances={x: Substance.from_formula(x) for x in 'MnO4-, C2O4-2, Fe+2, H+, Mn+2, CO2, Fe+3, H2O'.split(',')}
 
-print(substances)
 
-print(Reaction.from_string('MnO4- + C2O4-2 + Fe+2 + H+ -> Mn+2 + CO2 + Fe+3 + H2O', substances))
-# print(simple_balance("MnO4-, C2O4-2, Fe+2, H+" , "Mn+2, CO2, Fe+3, H2O"))
 
+def _command(
+        reactants: str, 
+        products: str
+        ) -> None:
+
+            try:
+                reac, prod = simple_balance(
+                    reactants=reactants,
+                    products=products
+                    )
+                reac, prod = map(dict, (reac, prod))
+            except ValueError as e:
+                formatted_str = format_reaction(
+                    {r:1 for r in reac}, 
+                    {p:1 for p in prod}
+                    )
+                return
+            else:
+                pass
+            formatted_str = format_reaction(reac, prod)
+            print(formatted_str)
+
+_command('H2O', 'H, O')
