d681fd34c0c69a08dcc2a8ac1b5ea60d31c16cf5
Eurydia <84164836+Eurydia@users.noreply.github.com>
2021-09-27 21:46:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from os import getenv
 
 from bot import ChemistryBot
-# from keep_alive import keep_alive
 
 def main():
-    #TODO: Error Message
-    #TODO: Other functions
-    #TODO: Remove duplicates
-    #TODO: Easier to access prefix
-    #TODO: Guessing product
-    #TODO: Error messages   
     #TODO: DEBUG mode
 
     bot = ChemistryBot('#')
+<<<<<<< Updated upstream
     # keep_alive()
+=======
+>>>>>>> Stashed changes
     bot.run(getenv('TOKEN'))
 
 if __name__ == '__main__':
