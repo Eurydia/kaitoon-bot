3f316170327db42f5cccedc9cd91ab2e9191ad38
Eurydia <84164836+Eurydia@users.noreply.github.com>
2021-11-29 09:08:18 +0700

    change how the input is formatted

--- chemistry/solvers/solver_chemequations.py
+++ chemistry/solvers/solver_chemequations.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from typing import Tuple, Union
 from json import loads
 from concurrent.futures import as_completed
@@ -9,68 +10,79 @@ from requests_futures.sessions import FuturesSession
 
 from chemistry.solvers.solver_base_class import ChemistrySolverBaseClass
 
+
 class ChemEquation(ChemistrySolverBaseClass):
     pass
-    
+
 
 class _ReactionPrediction(ChemEquation):
-    def solve(self, reactants: str='', products: str='', page_num: int=1) -> Embed:
+    def solve(
+        self, reactants: str = "", products: str = "", page_num: int = 1
+    ) -> Embed:
         if reactants is None and products is None:
             raise ValueError("Both reactants and products are empty.")
         if page_num < 1:
-            raise IndexError(f'Page {page_num} is out of range.')
+            raise IndexError(f"Page {page_num} is out of range.")
         timer_start = default_timer()
-        reactants = reactants.replace(",", " ").replace(" ", " + ")
-        products = products.replace(",", " ").replace(" ", " + ")
         page_num -= 1
-        query = self._prepare_query(reactants, products)
+        p_react = self._parse_input(reactants)
+        p_prod = self._parse_input(products)
+        query = self._prepare_query(p_react, p_prod)
         page_source = self._prepare_page_source(
-            'https://chemequations.com/en/advanced-search/?',
-            f'{query}&submit='
-            )
+            "https://chemequations.com/en/advanced-search/?", f"{query}&submit="
+        )
         result = self._format_page_source(page_source, page_num)
         timer_end = default_timer()
         return self._prepare_embed(
             result,
             (reactants, products),
-            f'\"{reactants}\" --> \"{products}\"',
-            timer_end - timer_start
+            f'"{p_react}" --> "{p_prod}"',
+            timer_end - timer_start,
         )
+
+    @staticmethod
+    def _parse_input(string: str) -> str:
+        string = string.replace(" + ", " ")
+        string = string.replace(",", " ")
+        string = " + ".join(map(lambda s: s.strip(), string.split()))
+        return string
 
     @staticmethod
     def _prepare_query(reactants: str, products: str) -> str:
         def _prepare(string: str, type: str) -> str:
             if not string:
-                return (f'{type}1=', )
-            string = string.replace(',', ' ').replace(' + ', ' ')
-            string = '&'.join(f'{type}{i}={f}' for i, f in enumerate(string.split(), 1))
+                return f"{type}1="
+            string = "&".join(f"{type}{i}={f}" for i, f in enumerate(string.split(), 1))
             return string
-        r = _prepare(reactants, 'reactant')
-        p = _prepare(products, 'product')
-        return f'{r}&{p}'
+
+        r = _prepare(reactants, "reactant")
+        p = _prepare(products, "product")
+        return f"{r}&{p}"
 
     @staticmethod
     def _prepare_page_source(base_url: str, query: str) -> str:
-            session = FuturesSession()
-            rqs = (session.get(f'{base_url}{query}'), )
-            content = ''
-            for req in as_completed(rqs):
-                resp = req.result()
-                content = resp.content.decode('utf-8')
-            return content
+        session = FuturesSession()
+        rqs = (session.get(f"{base_url}{query}"),)
+        content = ""
+        for req in as_completed(rqs):
+            resp = req.result()
+            content = resp.content.decode("utf-8")
+        return content
 
-    def _format_page_source(self, page_source: str, page_num: int) -> Union[Tuple, None]:
-        soup = BeautifulSoup(page_source, 'html.parser')
-        div_table = soup.find('div', {'class': 'search-results-async'})
+    def _format_page_source(
+        self, page_source: str, page_num: int
+    ) -> Union[Tuple, None]:
+        soup = BeautifulSoup(page_source, "html.parser")
+        div_table = soup.find("div", {"class": "search-results-async"})
         if not div_table:
             return None
         div_attrs = div_table.attrs
-        reactantids = div_attrs.get('data-reactantids')
-        productids = div_attrs.get('data-productids')
+        reactantids = div_attrs.get("data-reactantids")
+        productids = div_attrs.get("data-productids")
         offset = page_num * 10
         r = self._prepare_page_source(
-            'https://chemequations.com/api/search-reactions-by-compound-ids?',
-            f'reactantIds={reactantids}&productIds={productids}&offset={offset}'
+            "https://chemequations.com/api/search-reactions-by-compound-ids?",
+            f"reactantIds={reactantids}&productIds={productids}&offset={offset}",
         )
         json_dict = loads(r)
         results = self._prepare_result(json_dict)
@@ -79,50 +91,49 @@ class _ReactionPrediction(ChemEquation):
     @staticmethod
     def _prepare_result(json_dict: dict) -> Tuple[str, str, str]:
         formatted_equations = []
-        for equation in json_dict.get('searchResults'):
-            soup = BeautifulSoup(equation.get('equationStrBold', ''), 'html.parser')
-            eq = ' '.join(f'__{s.text.strip()}__' 
-                        if isinstance(s, element.Tag) else s.text.strip()
-                        for s in soup.childGenerator()
-                        )
-            eq = eq.replace(':', '')
+        for equation in json_dict.get("searchResults"):
+            soup = BeautifulSoup(equation.get("equationStrBold", ""), "html.parser")
+            eq = " ".join(
+                f"__{s.text.strip()}__"
+                if isinstance(s, element.Tag)
+                else s.text.strip()
+                for s in soup.childGenerator()
+            )
+            eq = eq.replace(":", "")
             formatted_equations.append(eq)
-        return json_dict.get('resultCount', 0), json_dict.get('offset', 0), tuple(formatted_equations)
-        
+        return (
+            json_dict.get("resultCount", 0),
+            json_dict.get("offset", 0),
+            tuple(formatted_equations),
+        )
+
     @staticmethod
     def _prepare_embed(
-        result: Tuple[str, Tuple[str]], 
-        inputs: Tuple[str], 
-        interpreted_as: str, 
-        time_elasped: float
+        result: Tuple[str, Tuple[str]],
+        inputs: Tuple[str],
+        interpreted_as: str,
+        time_elasped: float,
     ) -> Embed:
         embed = Embed()
         if not result:
-            embed.title = '😐 Something went wrong.'
+            embed.title = "😐 Something went wrong."
             embed.color = Colour.red()
-            embed.add_field(name='Status', value='❎ Failed', inline=False)
+            embed.add_field(name="Status", value="❎ Failed", inline=False)
         else:
             result_total, offset, eqs = result
-            embed.title = f'Found {result_total} equation(s)!'
+            embed.title = f"Found {result_total} equation(s)!"
             embed.color = Colour.green()
-            embed.description = f'Showing result {offset+1} - {((offset//10)+1)*10} of **{result_total}** equations\n'
-            embed.description += '\n'.join(f'{i}. {eq}\n' for i, eq in enumerate(eqs, offset+1))
-            embed.add_field(name='Status', value='✅ Success', inline=False)
-        embed.add_field(
-            name='Inputs',
-            value=' and '.join(f'" **{i}** "' for i in inputs),
-            inline=False
+            embed.description = f"Showing result {offset+1} - {((offset//10)+1)*10} of **{result_total}** equations\n"
+            embed.description += "\n".join(
+                f"{i}. {eq}\n" for i, eq in enumerate(eqs, offset + 1)
             )
+            embed.add_field(name="Status", value="✅ Success", inline=False)
         embed.add_field(
-            name='Interpreted as',
-            value=interpreted_as,
-            inline=False
-            )
-        embed.add_field(
-            name='Finished in',
-            value=f'{time_elasped:.2f}s',
-            inline=False
-            )
-        embed.set_footer(text='Powered by www.chemequations.com')
+            name="Inputs",
+            value=" and ".join(f'" **{i}** "' for i in inputs),
+            inline=False,
+        )
+        embed.add_field(name="Interpreted as", value=interpreted_as, inline=False)
+        embed.add_field(name="Finished in", value=f"{time_elasped:.2f}s", inline=False)
+        embed.set_footer(text="Powered by www.chemequations.com")
         return embed
-        
